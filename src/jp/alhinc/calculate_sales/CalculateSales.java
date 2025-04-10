b184052d99e6b138949e3a25b81336467c615714
inoue_saki <inoue.saki@alhinc.jp>
2025-04-10 11:06:59 +0900

    # エラー処理1 ・ 支店定義ファイルの存在チェック ・ 支店定義ファイルのフォーマットチェック
    # エラー処理2-1
    . 売上ファイルの連番チェック
    
    # エラー処理2-2
    ・ 合計金額の桁数チェック
    ・ 支店のコードの存在チェック
    ・ 売上ファイルのフォーマットチェック
    
    # エラー処理3
    ・ コマンドライン引数の個数チェック
    ・ 売上ファイルの種類チェック
    ・ 売上金額の数字チェック

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -23,6 +23,10 @@ public class CalculateSales {
 	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
 	private static final String FILE_NOT_EXIST = "支店定義ファイルが存在しません";
 	private static final String FILE_INVALID_FORMAT = "支店定義ファイルのフォーマットが不正です";
+	private static final String FILE_NONSEQENTIAL = "売上ファイル名が連番になっていません";
+	private static final String SALEAMOUNT_MAXLENGTH = "合計金額が10桁を超えました";
+	private static final String BRANCH_CODE_IS_INVALID = "の支店コードが不正です";
+	private static final String FORMAT_IS_INVALID = "のフォーマットが不正です";
 
 	/**
 	 * メインメソッド
@@ -30,6 +34,14 @@ public class CalculateSales {
 	 * @param コマンドライン引数
 	 */
 	public static void main(String[] args) {
+
+		// コマンドライン引数の存在チェック
+		// 売上集計システムファイルのパスが1つだけ設定されている想定
+		if (args.length != 1) {
+			System.out.println(UNKNOWN_ERROR);
+			return;
+		}
+
 		// 支店コードと支店名を保持するMap
 		Map<String, String> branchNames = new HashMap<>();
 		// 支店コードと売上金額を保持するMap
@@ -46,10 +58,23 @@ public class CalculateSales {
 		List<File> rcdFilesList = new ArrayList<>();
 
 		// 指定したディレクトリから、拡張子がrcd、且つファイル名が数字8桁のファイルを検索
+		// getNameメソッドはファイルとディレクトリの操作が可能であるため、ファイルであることを明示
 		for (int i = 0; i < files.length; i++) {
-			if (files[i].getName().matches("^[0-9]{8}[.]rcd$")) {
+			if (files[i].isFile() && files[i].getName().matches("^[0-9]{8}[.]rcd$")) {
 				// 該当したファイルを、売上ファイルとしてListに保持
 				rcdFilesList.add(files[i]);
+			}
+		}
+
+		// 売上ファイルの連番チェック
+		for (int i = 0; i < rcdFilesList.size() - 1; i++) {
+			int former = Integer.parseInt(rcdFilesList.get(i).getName().substring(0, 8));
+			int latter = Integer.parseInt(rcdFilesList.get(i + 1).getName().substring(0, 8));
+
+			// 比較する2つのファイル名の先頭から数字の8文字を切り出し、int型に変換
+			if ((latter - former) != 1) {
+				System.out.println(FILE_NONSEQENTIAL);
+				return;
 			}
 		}
 
@@ -82,10 +107,42 @@ public class CalculateSales {
 
 				// ファイル1つ分の情報がリストに入った状態
 
+				// Keyの存在チェック
+				// 1行目（支店コード）が支店定義ファイルに存在しない場合エラー
+				if (!branchNames.containsKey(contentsList.get(0))) {
+					System.out.println(file.getName() + BRANCH_CODE_IS_INVALID);
+					readPossibility = false;
+					return;
+				}
+
+				// 売上ファイルのフォーマットチェック
+				// 2行でない場合エラー
+				if (contentsList.size() != 2) {
+					System.out.println(file.getName() + FORMAT_IS_INVALID);
+					readPossibility = false;
+					return;
+				}
+
+				// 売上金額の数字チェック
+				// Long型にキャストする前に行う必要がある
+				if (!contentsList.get(1).matches("^[0-9]+$")) {
+					System.out.println(UNKNOWN_ERROR);
+					readPossibility = false;
+					return;
+				}
+
 				// ファイルの2行目（売上金額）をString型からLong型にキャスト
 				long fileSale = Long.parseLong(contentsList.get(1));
+
 				// ファイルの1行目（支店コード）をキーに合計売上金額を取得し、個別の売上金額と加算
 				long saleAmount = branchSales.get(contentsList.get(0)) + fileSale;
+				// 合計金額の最大桁チェック（10桁超でアウト）
+				if (saleAmount >= 10000000000L) {
+					System.out.println(SALEAMOUNT_MAXLENGTH);
+					readPossibility = false;
+					return;
+				}
+
 				// 合計売上金額をMapにセット
 				branchSales.put(contentsList.get(0), saleAmount);
 
@@ -131,6 +188,12 @@ public class CalculateSales {
 
 		try {
 			File file = new File(path, fileName);
+			// ファイルの存在チェック
+			if (!file.exists()) {
+				System.out.println(FILE_NOT_EXIST);
+				return false;
+			}
+
 			FileReader fr = new FileReader(file);
 			br = new BufferedReader(fr);
 
@@ -140,6 +203,11 @@ public class CalculateSales {
 				// 処理内容1-2
 				// items[0] には支店コード、items[1] には支店名を格納
 				String[] items = line.split(",");
+				//ファイルのフォーマットチェック
+				if ((items.length != 2) || !(items[0].matches("^[0-9]{3}"))) {
+					System.out.println(FILE_INVALID_FORMAT);
+					return false;
+				}
 
 				// 支店情報（支店コード、支店名）を格納
 				branchNames.put(items[0], items[1]);
